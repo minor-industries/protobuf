d32f806c0ddc812ba27da0f3877a5dd2f2397b9e
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-08-08 21:48:56 -0700

    initial commit

---
+++ make/rules.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/minor-industries/grm"
+	"path/filepath"
+)
+
+var rules = map[string]func(rule string){
+	"protos": func(rule string) {
+		protoFiles, err := filepath.Glob("protos/*/*.proto")
+		noError(err)
+
+		args := []string{
+			"/bin/protoc",
+			"--proto_path=./protos/heads",
+			"-I/build/include",
+			"--go_out=plugins=grpc,paths=source_relative:./gen/go/heads",
+		}
+
+		for _, file := range protoFiles {
+			// this may run into trouble if there are two proto files with the same name in
+			// different directories
+			base := filepath.Base(file)
+			opt := fmt.Sprintf("--go_opt=M%s=github.com/cacktopus/theheads/gen/go/heads", base)
+			args = append(args, opt)
+		}
+
+		args = append(args, protoFiles...)
+
+		grm.RunDocker("heads-protoc", args...)
+	},
+
+	"heads-protoc": func(rule string) {
+		grm.Cd("common", func() {
+			grm.Run(nil, "docker", "build", "--tag", "heads-protoc", "protos")
+		})
+	},
+}
+
+func noError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	grm.Main(rules)
+}
